ba689b0086ae768aa4783c3b7ee7dc02bdf6c482
alialabdrabalrasol <alialabdrabalrasol@gmail.com>
2022-05-18 18:12:46 +0300

    Homework Answers

---
+++ Homework03/src/Circle.java
@@ -0,0 +1,39 @@
+public class Circle extends Shape{
+    protected double radius=1.0;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, Boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ Homework03/src/Square.java
@@ -0,0 +1,37 @@
+public class Square extends Rectangle{
+    protected double side;
+    public Square(){
+
+    }
+    public Square(double side){
+        this.side=side;
+    }
+    public Square(double side,String color,Boolean filled){
+
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+    public void setWidth(double side){
+        super.setWidth(side);
+    }
+    public void setLength(double side){
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
